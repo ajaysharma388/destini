854e10326102d918f7211a4328bde48ef5cf0b02
AJAY SHARMA <ajaysharma2201@gmail.com>
2019-09-16 00:06:30 +0530

    update-2019.09.16.1

---
+++ lib/entrypoint.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[300],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[],
+      ),
+    );
+  }
+}
